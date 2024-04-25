0b5bedfd18abf76ad38dcf6ec0e7183ad44fadfc
mxaze <narcie.nar@gmail.com>
2024-04-25 15:06:19 +0200

    oi

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const Database = require("better-sqlite3");
 const bcrypt = require("bcrypt");
-const db = new Database("dev.db");
+const db = new Database("my.db");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -13,34 +13,26 @@ const insertUser = db.prepare(
 );
 
 app.post("/login", (req, res) => {
-  const { mail, password } = req.body;
-  const user = db.prepare("SELECT * FROM users WHERE mail = ?").get(mail);
-  if (!user) {
-    res.status(401).send("Invalid email or password");
-    return;
-  }
-  const compare = bcrypt.compareSync(password, user.password);
-
-  if (compare) {
-    res.redirect("/");
-  } else {
-    res.status(401).send("Invalid email or password");
-  }
+  const { mail, password } = req.body; // gets the email and password from the request body
+  const user = db.prepare("SELECT * FROM users WHERE email = ?").get(mail); // gets the user with the given email
 });
 
 app.post("/register", (req, res) => {
   const { name, mail, phone, password } = req.body; // gets the email and password from the request body
-  const user = db.prepare("SELECT * FROM users WHERE mail = ?").get(mail); // gets the user from the database
-
-  if (user) {
-    res.status(409).send("Email already exists"); // if the user exists, send a 409 status code
-  } else {
-    // hash the password
-    const hash = bcrypt.hashSync(password, 6);
-    insertUser.run(name, mail, phone, hash); // inserts the user into the database
-    res.redirect("/") // sends the user to main site
-  }
+  const user = db.prepare("SELECT * FROM users WHERE email = ?").get(email); // gets the user with the given email
 });
+
+if (user) {
+  res.status(409).send("Email already exists"); // if the user exists, send a 409 status code
+} else {
+  // hash the password
+  const hash = bcrypt.hashSync(password, 6);
+
+  // inserts the user into the database
+  const insertUser = () => {
+    sql.insertUser.run(name, email, rolle, hash, "", "", "", "1", "0", token);
+  };
+}
 
 app.get("/register", (req, res) => {
   res.sendFile(__dirname + "/public/register.html");
